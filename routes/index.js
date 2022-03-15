6b820e700e53dacaf43d362ed7fcd16161381a4e
Jing Liu <jingliu778@gmail.com>
2022-03-14 20:41:36 -0400

    set apiroute

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
-
+// set wrong route
 router.use((req, res) => {
   res.status(404).send("Wrong route!");
 });
