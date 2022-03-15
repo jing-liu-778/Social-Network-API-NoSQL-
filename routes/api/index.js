495b126e3d1b562435d67518323ba6606425e65b
Jing Liu <jingliu778@gmail.com>
2022-03-14 20:40:47 -0400

    set routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const thoughtRoutes = require("./thoughtRoutes");
-
+// set routes
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
 
